9caed741b3e5de7fe4a62acf4ddec2a0de230c74
Eugene Petrik <eugeniy.petrik@gmail.com>
2024-07-08 14:29:36 +0300

    Update BoardController

--- api/api_tests3/src/controllers/BoardController.ts
+++ api/api_tests3/src/controllers/BoardController.ts
@@ -1,36 +1,30 @@
 import { type AxiosResponse } from 'axios';
 import AxiosController, { type RequestOptions } from './AxiosController';
+import { withAuth } from '../utils/auth';
 import { BaseConfig } from '../utils/BaseConfig';
 
 export class BoardController extends AxiosController {
-  private apiKey: string;
-
-  private apiToken: string;
-
   constructor(options: RequestOptions = { baseUrl: BaseConfig.baseUrl }) {
     super(options);
-    this.apiKey = BaseConfig.trelloApiKey;
-    this.apiToken = BaseConfig.trelloApiToken;
-  }
-
-  private withAuth(): this {
-    this.searchParams({ key: this.apiKey, token: this.apiToken });
-    return this;
-  }
-
-  public async createBoard(name: string): Promise<AxiosResponse<any>> {
-    return this.searchParams({ name }).withAuth().req<any>('POST', '/boards/');
   }
 
   public async getBoard(id: string): Promise<AxiosResponse<any>> {
-    return this.withAuth().req<any>('GET', `/boards/${id}`);
+    const options = withAuth(this.options);
+    return this.req<any>('GET', `/boards/${id}`, options);
+  }
+
+  public async createBoard(name: string): Promise<AxiosResponse<any>> {
+    const options = withAuth(this.searchParams({ name }).options);
+    return this.req<any>('POST', '/boards/', options);
   }
 
   public async updateBoard(id: string, data: any): Promise<AxiosResponse<any>> {
-    return this.withAuth().body(data).req<any>('PUT', `/boards/${id}`);
+    const options = withAuth(this.body(data).options);
+    return this.req<any>('PUT', `/boards/${id}`, options);
   }
 
   public async deleteBoard(id: string): Promise<AxiosResponse<any>> {
-    return this.withAuth().req<any>('DELETE', `/boards/${id}`);
+    const options = withAuth(this.options);
+    return this.req<any>('DELETE', `/boards/${id}`, options);
   }
 }
