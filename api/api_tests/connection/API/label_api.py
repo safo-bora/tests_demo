ef79614a928b5454f72bd3824be3a98455be20f8
ShulhaVitalii <vetalik91@ukr.net>
2024-07-09 14:43:18 +0300

    Add CRUD test for labels

--- api/api_tests/connection/API/label_api.py
+++ api/api_tests/connection/API/label_api.py
@@ -1,3 +1,4 @@
+from api.api_tests.connection.lib import Label
 from api.api_tests.connection.trello_connection import TrelloAPI
 from log_lib import logger
 
@@ -14,3 +15,36 @@ class LabelAPI(TrelloAPI):
         else:
             logger.error(f"Error deleting label: {response.status_code} - {response.text}")
             raise Exception
+
+    def create_label(self, id_board, label_name="Test name", color="red"):
+        params = {
+            'name': label_name,
+            'color': color,
+            'idBoard': id_board,
+            **self.auth_params
+        }
+        response = self.http.post("labels", params=params)
+        print(response)
+        response.raise_for_status()
+        label_data = response.json()
+        return Label(label_data)
+
+    def get_label(self, label_id):
+        response = self.http.get(f"labels/{label_id}", params=self.auth_params)
+        response.raise_for_status()
+        label_data = response.json()
+        return Label(label_data)
+
+    def update_label(self, label_id, new_name):
+        params = {'name': new_name, **self.auth_params}
+        response = self.http.put(f"labels/{label_id}", params=params)
+        response.raise_for_status()
+        updated_label_data = response.json()
+        return Label(updated_label_data)
+
+    def update_field_on_a_label(self, label_id, field_id, value):
+        params = {'value': value, **self.auth_params}
+        response = self.http.put(f"labels/{label_id}/{field_id}", params=params)
+        response.raise_for_status()
+        updated_field_data = response.json()
+        return updated_field_data
